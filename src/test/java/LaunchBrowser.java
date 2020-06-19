7a7a18d17470cefeaba700d353cf108a3447d86b
Karthick M <karthick.m@otr-ict.chn>
2020-06-19 20:36:38 +0530

    first

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -1,8 +1,6 @@
 
 public class LaunchBrowser {
 public static void main(String[] args) {
-	System.out.println("update for employee1");
-	System.out.println("change");
 	System.out.println("Welcome to facebook");
 }
 }
